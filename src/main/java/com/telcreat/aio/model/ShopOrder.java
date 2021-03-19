235ef594537ef057bbac1732aaf2bbc407cbd2b2
Xabier Aresti <xabier.aresti@gmail.com>
2021-03-19 20:37:48 +0100

    Order.java renamed to ShopOrder.java

--- src/main/java/com/telcreat/aio/model/ShopOrder.java
+++ src/main/java/com/telcreat/aio/model/ShopOrder.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Order {
+public class ShopOrder {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
