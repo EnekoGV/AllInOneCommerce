5bdfad9edffffe597e734572efccf7732590ec89
Xabier Aresti <xabier.aresti@gmail.com>
2021-04-13 16:30:35 +0200

    Basic viewController.java configuration.

--- src/main/java/com/telcreat/aio/viewController/viewController.java
+++ src/main/java/com/telcreat/aio/viewController/viewController.java
@@ -1,7 +1,29 @@
 package com.telcreat.aio.viewController;
 
+import com.telcreat.aio.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class viewController {
+
+    private final CartService cartService;
+    private final ItemService itemService;
+    private final PictureService pictureService;
+    private final ShopOrderService shopOrderService;
+    private final UserService userService;
+    private final VariantService variantService;
+
+
+    @Autowired
+    public viewController(CartService cartService, ItemService itemService, PictureService pictureService, ShopOrderService shopOrderService, UserService userService, VariantService variantService) {
+        this.cartService = cartService;
+        this.itemService = itemService;
+        this.pictureService = pictureService;
+        this.shopOrderService = shopOrderService;
+        this.userService = userService;
+        this.variantService = variantService;
+    }
+
+
 }
