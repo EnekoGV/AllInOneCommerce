24d9d84efee8e670ec2960841ae962fe349ae5ca
Xabier Aresti <xabier.aresti@gmail.com>
2021-04-14 10:02:24 +0200

    Added CategoryService.findAllCategories method

--- src/main/java/com/telcreat/aio/service/CategoryService.java
+++ src/main/java/com/telcreat/aio/service/CategoryService.java
@@ -1,7 +1,23 @@
 package com.telcreat.aio.service;
 
+import com.telcreat.aio.model.Category;
+import com.telcreat.aio.repo.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryService {
+
+    private final CategoryRepo categoryRepo;
+
+    @Autowired
+    public CategoryService(CategoryRepo categoryRepo) {
+        this.categoryRepo = categoryRepo;
+    }
+
+    public List<Category> findAllCategories(){
+        return categoryRepo.findAll();
+    }
 }
