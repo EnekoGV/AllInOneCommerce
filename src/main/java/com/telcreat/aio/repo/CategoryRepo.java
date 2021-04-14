140cf4ef5bfb395eafd937b7f13724f7e271d47d
Xabier Aresti <xabier.aresti@gmail.com>
2021-04-14 09:59:51 +0200

    Added @Repository

--- src/main/java/com/telcreat/aio/repo/CategoryRepo.java
+++ src/main/java/com/telcreat/aio/repo/CategoryRepo.java
@@ -2,6 +2,8 @@ package com.telcreat.aio.repo;
 
 import com.telcreat.aio.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface categoryRepo extends JpaRepository<Category, Integer> {
+@Repository
+public interface CategoryRepo extends JpaRepository<Category, Integer> {
 }
