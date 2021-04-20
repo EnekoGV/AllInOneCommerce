908f40ab1d9f4a42222239321b2025bf22334edd
Xabier Aresti <xabier.aresti@gmail.com>
2021-04-20 10:27:17 +0200

    Added Password Recovery Link in the Verification email

--- src/main/java/com/telcreat/aio/service/SendEmail.java
+++ src/main/java/com/telcreat/aio/service/SendEmail.java
@@ -1,6 +1,8 @@
 package com.telcreat.aio.service;
 
 
+import com.telcreat.aio.model.VerificationToken;
+
 import java.util.Properties;
 
 import javax.mail.Message;
@@ -13,9 +15,9 @@ import javax.mail.internet.MimeMessage;
 
 public class SendEmail {
 
-    public void send(String email, String code) {
+    public void send(String email, VerificationToken verificationToken) {
 
-        final String remitente = "aio@telcreat.com";
+        final String sender = "aio@telcreat.com";
         final String password = "Es8qYcGcpmvs";
 
         Properties props = new Properties();
@@ -27,21 +29,23 @@ public class SendEmail {
 
         Session session = Session.getInstance(props, new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(remitente, password);
+                return new PasswordAuthentication(sender, password);
             }
         });
 
         try {
             Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(remitente));
+            message.setFrom(new InternetAddress(sender));
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-            message.setSubject("Verification code");
-            message.setText("Your verification code is: " + code);
+            message.setSubject("Verification is required! AIO Commerce");
+
+            String passwordRecoveryLink = "\n\n\nIf you think there is something wrong, change your password: http://localhost:8080/auth/recoverPassword?token=" + verificationToken.getToken() + "&code=" + verificationToken.getCode();
+            message.setText("Your verification code is: " + verificationToken.getCode() + passwordRecoveryLink);
 
             // Send email
             Transport.send(message);
 
-            System.out.println("Sent message successfully....");
+            System.out.println("Verification message sent successfully....");
 
         } catch (MessagingException e) {
             throw new RuntimeException(e);
