00a8a7df651f02e77944432156c3d1c117969031
Xabier Aresti <xabier.aresti@gmail.com>
2021-04-25 15:26:00 +0200

    ItemRepo: added new DB query (ItemRepo.findItemByIdAndStatus) to ItemRepo.java

--- src/main/java/com/telcreat/aio/repo/ItemRepo.java
+++ src/main/java/com/telcreat/aio/repo/ItemRepo.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ItemRepo  extends JpaRepository<Item, Integer> {
     List<Item> findItemsByItemCategory_IdAndNameIsContaining(int itemCategoryId,String itemName);
     List<Item> findItemsByShop_Id(int shopId);
+    Optional<Item> findItemByIdAndStatus(int itemId, Item.Status itemStatus);
 }
