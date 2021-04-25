03ce9d49844e8279fd5adeb721fb0b7bc08cc952
Orbe <u.orbe.deusto@gmail.com>
2021-04-25 23:00:37 +0200

    Logica Pricipal de ver y editar Cart, y parte de la logica de Order

--- src/main/java/com/telcreat/aio/repo/ShopOrderRepo.java
+++ src/main/java/com/telcreat/aio/repo/ShopOrderRepo.java
@@ -4,6 +4,10 @@ import com.telcreat.aio.model.ShopOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ShopOrderRepo extends JpaRepository<ShopOrder, Integer> {
+    List<ShopOrder> findShopOrderByUserId(int userId);
 }
--- src/main/java/com/telcreat/aio/viewController/cartController.java
+++ src/main/java/com/telcreat/aio/viewController/cartController.java
@@ -1,17 +1,23 @@
 package com.telcreat.aio.viewController;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import com.telcreat.aio.model.Cart;
+import com.telcreat.aio.model.ShopOrder;
 import com.telcreat.aio.model.User;
 import com.telcreat.aio.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.annotation.RequestScope;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequestScope
@@ -21,6 +27,8 @@ public class cartController {
     private final UserService userService;
     private final ShopService shopService;
     private final CartService cartService;
+    private final ShopOrderService shopOrderService;
+    private final VariantService variantService;
 
     private User loggedUser;
     private boolean isLogged = false;
@@ -37,6 +45,8 @@ public class cartController {
         this.cartService = cartService;
 
         this.loggedUser = userService.getLoggedUser();
+        this.shopOrderService = shopOrderService;
+        this.variantService = variantService;
         if (this.loggedUser != null){
             isLogged = true;
             loggedId = loggedUser.getId();
@@ -47,10 +57,9 @@ public class cartController {
     }
 
     @RequestMapping(value = "/cart", method = RequestMethod.GET)
-    public String viewCart(@RequestParam(name = "cartId")int cartId,
-                           @RequestParam(name = "userId")int userId,
+    public String viewCart(@RequestParam(name = "userId")int userId,
                            ModelMap modelMap){
-        Cart cart = cartService.findCartById(cartId);
+        Cart cart = cartService.findCartByUserId(userId);
         if (cart != null && isLogged && loggedId == userId && cart.getUser().getId() == loggedId){ // Allow editing only each user's profile.
 
             // DEFAULT INFORMATION IN ALL VIEWS
@@ -60,9 +69,70 @@ public class cartController {
             modelMap.addAttribute("loggedCartId", loggedCartId);
 
            modelMap.addAttribute("cart", cart);
+
             return "cart";
         }else{
             return "redirect:/";
         }
+    }
+
+    @RequestMapping(value = "/cart/eliminate", method = RequestMethod.POST)
+    public String eliminateCartItem(@RequestParam(name = "variantId")int variantId,
+                                       @RequestParam(name = "cartId")int cartId){
+        Cart cart = cartService.findCartById(cartId);
+        if(cart != null && cart.getUser().getId() == loggedUser.getId()){
+            for(int i=0; i<cart.getVariants().size(); i++){
+                if(variantId == cart.getVariants().get(i).getId())
+                    cart.getVariants().remove(i);
+            }
+            cartService.updateCart(cart);
+            return "redirect:/cart";
+        }else
+            return "redirect:/";
+    }
+
+    @RequestMapping(value = "/cart/add", method = RequestMethod.POST)
+    public String addItem(@RequestParam(name = "variantId")int variantId,
+                          @RequestParam(name = "cartId")int cartId){
+        Cart cart = cartService.findCartById(cartId);
+        if(cart != null && cart.getUser().getId() == loggedUser.getId()){
+            cart.getVariants().add(variantService.findVariantById(variantId));
+            cartService.updateCart(cart);
+            return "redirect:/cart";
+        }else
+            return "redirect:/";
+    }
+
+    @RequestMapping(value = "/cart/order", method = RequestMethod.POST)
+    public String createOrder(@ModelAttribute(name = "cart")Cart cart,
+                              @RequestParam(name = "userId")int userId){
+        if(cart != null && cart.getUser().getId() == userId && loggedId == userId){
+            List<ShopOrder> shopOrders= shopOrderService.createShopOrderFromCart(cart);
+            if(shopOrders == null)
+                return "redirect:/";
+            else
+                return "redirect:/cart/order";
+        }else
+            return "redirect:/";
+    }
+
+    @RequestMapping(value = "/cart/order", method = RequestMethod.GET)
+    public String viewOrders(@RequestParam(name = "userId")int userId,
+                             ModelMap modelMap){
+        List<ShopOrder> shopOrders = shopOrderService.findShopOrdersByUserId(userId);
+        if (shopOrders != null && isLogged && loggedId == userId){
+
+            // DEFAULT INFORMATION IN ALL VIEWS
+            modelMap.addAttribute("isLogged", isLogged);
+            modelMap.addAttribute("loggedUserId", loggedId);
+            modelMap.addAttribute("loggedUserRole", loggedRole);
+            modelMap.addAttribute("loggedCartId", loggedCartId);
+
+            modelMap.addAttribute("ordersList", shopOrders);
+            return "orders";
+        }else{
+            return "redirect:/";
+        }
+
     }
 }
