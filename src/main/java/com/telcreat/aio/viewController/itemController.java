6f5a44dd5b5b9184385379b5e8a3643ecb089f99
Xabier Aresti <xabier.aresti@gmail.com>
2021-04-27 09:14:36 +0200

    Moved Item methods to itemController.java from shopController.java

--- src/main/java/com/telcreat/aio/viewController/itemController.java
+++ src/main/java/com/telcreat/aio/viewController/itemController.java
@@ -1,10 +1,18 @@
 package com.telcreat.aio.viewController;
 
+import com.telcreat.aio.model.Item;
+import com.telcreat.aio.model.Picture;
+import com.telcreat.aio.model.Shop;
 import com.telcreat.aio.model.User;
 import com.telcreat.aio.service.*;
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.annotation.RequestScope;
 
 @Data
@@ -43,6 +51,125 @@ public class itemController {
         }
         this.fileUploaderService = fileUploaderService;
         this.variantService = variantService;
+    }
+
+    @RequestMapping(value = "/item", method = RequestMethod.GET)
+    public String viewItem(@RequestParam(name = "itemId") int itemId,
+                           ModelMap modelMap){
+
+        Item item = itemService.findActiveItemById(itemId);
+
+        if (item != null){
+
+            // DEFAULT INFORMATION IN ALL VIEWS
+            modelMap.addAttribute("isLogged", isLogged);
+            modelMap.addAttribute("loggedUserId", loggedId);
+            modelMap.addAttribute("loggedUserRole", loggedRole);
+            modelMap.addAttribute("isOwner", isOwner);
+
+            modelMap.addAttribute("item", item);
+            modelMap.addAttribute("shop", item.getShop());
+
+            return "item"; // Return Item view
+        }
+        else{
+            return "redirect:/?itemNotFound";
+        }
+    }
+
+    @RequestMapping(value = "/item/edit/create", method = RequestMethod.GET)
+    public String createItem(@RequestParam(name = "shopId") int shopId,
+                             ModelMap modelMap){
+
+        Shop shop = shopService.findActiveShopById(shopId);
+
+        if (isLogged && shop != null && loggedId == shop.getOwner().getId()){
+            Picture newPicture = new Picture("/images/Item.png");
+            Picture savedPicture = pictureService.createPicture(newPicture);
+            Item newItem = new Item(shop, null, savedPicture, "", "", 0.0f, "", Item.Status.ACTIVE);
+            Item savedItem = itemService.createItem(newItem);
+
+            return "redirect:/item/edit?itemId=" + savedItem.getId();
+        }
+        else{
+            return "redirect:/?errorCreatingItem";
+        }
+    }
+
+    @RequestMapping(value = "/item/edit", method = RequestMethod.GET)
+    public String viewEditItem(@RequestParam(name = "itemId") int itemId,
+                               @RequestParam(name = "editVariantNumber", required = false, defaultValue = "0") int editVariantNumber,
+                               @RequestParam(name = "edit", required = false, defaultValue = "false") boolean edit,
+                               @RequestParam(name = "itemUpdateError", required = false, defaultValue = "false") boolean itemUpdateError,
+                               @RequestParam(name = "variantUpdateError", required = false, defaultValue = "false") boolean variantUpdateError,
+                               @RequestParam(name = "variantDeleteError", required = false, defaultValue = "false") boolean variantDeleteError,
+                               ModelMap modelMap){
+
+        Item item = itemService.findActiveItemById(itemId);
+
+        if (isLogged && item != null && loggedId == item.getShop().getOwner().getId()){
+
+            // DEFAULT INFORMATION IN ALL VIEWS
+            modelMap.addAttribute("isLogged", isLogged);
+            modelMap.addAttribute("loggedUserId", loggedId);
+            modelMap.addAttribute("loggedUserRole", loggedRole);
+            modelMap.addAttribute("isOwner", isOwner);
+
+            modelMap.addAttribute("item", item);
+            modelMap.addAttribute("variantList", variantService.findActiveVariantsByItemId(item.getId()));
+
+            modelMap.addAttribute("editVariantNumber", editVariantNumber);
+            modelMap.addAttribute("edit", edit);
+            modelMap.addAttribute("itemUpdateError", itemUpdateError);
+            modelMap.addAttribute("variantUpdateError", variantUpdateError);
+            modelMap.addAttribute("variantDeleteError", variantDeleteError);
+
+            return "addProduct";
+        }
+        else{
+            return "redirect:/?notAllowed";
+        }
+    }
+
+    @RequestMapping(value = "/item/edit", method = RequestMethod.POST)
+    public String receiveEditItem(@ModelAttribute(name = "item") Item itemForm,
+                                  ModelMap modelMap){
+
+        modelMap.clear();
+
+        Item item = itemService.findActiveItemById(itemForm.getId());
+
+        if (isLogged && item != null && loggedId == item.getShop().getOwner().getId()){
+            Item savedItem = itemService.updateItem(itemForm);
+            if (savedItem != null){
+                return "redirect:/item/edit?itemId=" + item.getId();
+            }
+            else{
+                return "redirect:/item/edit?itemId=" + item.getId() + "&itemUpdateError=true";
+            }
+        }
+        else{
+            return "redirect:/?notAllowed";
+        }
+    }
+
+    @RequestMapping(value = "/item/edit/delete", method = RequestMethod.POST)
+    public String deactivateItem(@RequestParam(name = "itemId") int itemId,
+                                 ModelMap modelMap){
+        Item item = itemService.findActiveItemById(itemId);
+        boolean control;
+        if (isLogged && item != null && loggedId == item.getShop().getOwner().getId()){
+            control = itemService.deactivateItem(item.getId());
+            if (control){
+                return "redirect:/shop?shopId=" + item.getShop().getId();
+            }
+            else{
+                return "redirect:/item/edit?itemId=" + item.getId() + "&variantDeleteError=true";
+            }
+        }
+        else{
+            return "redirect:/?notAllowed";
+        }
     }
 
 }
--- src/main/java/com/telcreat/aio/viewController/shopController.java
+++ src/main/java/com/telcreat/aio/viewController/shopController.java
@@ -234,31 +234,6 @@ public class shopController {
         }
     }
 
-    @RequestMapping(value = "/item", method = RequestMethod.GET)
-    public String viewItem(@RequestParam(name = "itemId") int itemId,
-                           ModelMap modelMap){
-
-        Item item = itemService.findActiveItemById(itemId);
-
-        if (item != null){
-
-            // DEFAULT INFORMATION IN ALL VIEWS
-            modelMap.addAttribute("isLogged", isLogged);
-            modelMap.addAttribute("loggedUserId", loggedId);
-            modelMap.addAttribute("loggedUserRole", loggedRole);
-            modelMap.addAttribute("isOwner", isOwner);
-
-            modelMap.addAttribute("item", item);
-            modelMap.addAttribute("shop", item.getShop());
-
-            return "item"; // Return Item view
-        }
-        else{
-            return "redirect:/?itemNotFound";
-        }
-    }
-
-
     @RequestMapping(value = "/shop/edit/uploadPicture", method = RequestMethod.POST)
     public String uploadUserPicture(@RequestParam(name = "shopPicture") MultipartFile file,
                                     @RequestParam(name = "shopId") int shopId,
@@ -303,83 +278,6 @@ public class shopController {
 
     }
 
-
-    @RequestMapping(value = "/item/edit/create", method = RequestMethod.GET)
-    public String createItem(@RequestParam(name = "shopId") int shopId,
-                             ModelMap modelMap){
-
-        Shop shop = shopService.findActiveShopById(shopId);
-
-        if (isLogged && shop != null && loggedId == shop.getOwner().getId()){
-            Picture newPicture = new Picture("/images/Item.png");
-            Picture savedPicture = pictureService.createPicture(newPicture);
-            Item newItem = new Item(shop, null, savedPicture, "", "", 0.0f, "", Item.Status.ACTIVE);
-            Item savedItem = itemService.createItem(newItem);
-
-            return "redirect:/item/edit?itemId=" + savedItem.getId();
-        }
-        else{
-            return "redirect:/?errorCreatingItem";
-        }
-    }
-
-    @RequestMapping(value = "/item/edit", method = RequestMethod.GET)
-    public String viewEditItem(@RequestParam(name = "itemId") int itemId,
-                           @RequestParam(name = "editVariantNumber", required = false, defaultValue = "0") int editVariantNumber,
-                           @RequestParam(name = "edit", required = false, defaultValue = "false") boolean edit,
-                           @RequestParam(name = "itemUpdateError", required = false, defaultValue = "false") boolean itemUpdateError,
-                           @RequestParam(name = "variantUpdateError", required = false, defaultValue = "false") boolean variantUpdateError,
-                           @RequestParam(name = "variantDeleteError", required = false, defaultValue = "false") boolean variantDeleteError,
-                           ModelMap modelMap){
-
-        Item item = itemService.findActiveItemById(itemId);
-
-        if (isLogged && item != null && loggedId == item.getShop().getOwner().getId()){
-
-            // DEFAULT INFORMATION IN ALL VIEWS
-            modelMap.addAttribute("isLogged", isLogged);
-            modelMap.addAttribute("loggedUserId", loggedId);
-            modelMap.addAttribute("loggedUserRole", loggedRole);
-            modelMap.addAttribute("isOwner", isOwner);
-
-            modelMap.addAttribute("item", item);
-            modelMap.addAttribute("variantList", variantService.findActiveVariantsByItemId(item.getId()));
-
-            modelMap.addAttribute("editVariantNumber", editVariantNumber);
-            modelMap.addAttribute("edit", edit);
-            modelMap.addAttribute("itemUpdateError", itemUpdateError);
-            modelMap.addAttribute("variantUpdateError", variantUpdateError);
-            modelMap.addAttribute("variantDeleteError", variantDeleteError);
-
-            return "addProduct";
-        }
-        else{
-            return "redirect:/?notAllowed";
-        }
-    }
-
-    @RequestMapping(value = "/item/edit", method = RequestMethod.POST)
-    public String receiveEditItem(@ModelAttribute(name = "item") Item itemForm,
-                                  ModelMap modelMap){
-
-        modelMap.clear();
-
-        Item item = itemService.findActiveItemById(itemForm.getId());
-
-        if (isLogged && item != null && loggedId == item.getShop().getOwner().getId()){
-            Item savedItem = itemService.updateItem(itemForm);
-            if (savedItem != null){
-                return "redirect:/item/edit?itemId=" + item.getId();
-            }
-            else{
-                return "redirect:/item/edit?itemId=" + item.getId() + "&itemUpdateError=true";
-            }
-        }
-        else{
-            return "redirect:/?notAllowed";
-        }
-    }
-
     @RequestMapping(value = "/variant/edit", method = RequestMethod.POST)
     public String receiveEditVariant(@ModelAttribute(name = "variant") Variant variantForm,
                                      ModelMap modelMap){
@@ -419,24 +317,4 @@ public class shopController {
             return "redirect:/?notAllowed";
         }
     }
-
-    @RequestMapping(value = "/item/edit/delete", method = RequestMethod.POST)
-    public String deactivateItem(@RequestParam(name = "itemId") int itemId,
-                                    ModelMap modelMap){
-        Item item = itemService.findActiveItemById(itemId);
-        boolean control;
-        if (isLogged && item != null && loggedId == item.getShop().getOwner().getId()){
-            control = itemService.deactivateItem(item.getId());
-            if (control){
-                return "redirect:/shop?shopId=" + item.getShop().getId();
-            }
-            else{
-                return "redirect:/item/edit?itemId=" + item.getId() + "&variantDeleteError=true";
-            }
-        }
-        else{
-            return "redirect:/?notAllowed";
-        }
-    }
-
 }
