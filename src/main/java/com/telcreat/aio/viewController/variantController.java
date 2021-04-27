df3c8c67f8b888ae26449e6bbe58f7c031989249
Xabier Aresti <xabier.aresti@gmail.com>
2021-04-27 09:15:51 +0200

    Created variantController.java

---
+++ src/main/java/com/telcreat/aio/viewController/variantController.java
@@ -0,0 +1,48 @@
+package com.telcreat.aio.viewController;
+
+import com.telcreat.aio.model.User;
+import com.telcreat.aio.service.*;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Data
+@RequestScope
+@Controller
+public class variantController {
+
+    private final ItemService itemService;
+    private final PictureService pictureService;
+    private final UserService userService;
+    private final ShopService shopService;
+    private final FileUploaderService fileUploaderService;
+    private final VariantService variantService;
+
+    private User loggedUser;
+    private boolean isLogged = false;
+    private User.UserRole loggedRole = User.UserRole.CLIENT;
+    private int loggedId;
+    private boolean isOwner = false;
+
+    @Autowired
+    public variantController(ItemService itemService, PictureService pictureService, UserService userService, FileUploaderService fileUploaderService, ShopService shopService, VariantService variantService) {
+        this.itemService = itemService;
+        this.pictureService = pictureService;
+        this.userService = userService;
+        this.shopService = shopService;
+
+        loggedUser = userService.getLoggedUser();
+        if (loggedUser != null){
+            isLogged = true;
+            loggedId = loggedUser.getId();
+            loggedRole = loggedUser.getUserRole();
+            if (loggedRole == User.UserRole.OWNER){
+                isOwner = true;
+            }
+        }
+        this.fileUploaderService = fileUploaderService;
+        this.variantService = variantService;
+    }
+
+}
