ff939e0b5a0199f90f81cb3d0ee70a65fc9dc1c6
Xabier Aresti <xabier.aresti@gmail.com>
2021-04-27 19:48:51 +0200

    Created ItemEditForm.java

---
+++ src/main/java/com/telcreat/aio/model/ItemEditForm.java
@@ -0,0 +1,18 @@
+package com.telcreat.aio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ItemEditForm {
+
+    private int id;
+    private String shortDescription;
+    private String longDescription;
+    private Float price;
+    private String name;
+}
